13e115d879b1d38ad830058847324017fb095fc4
Jefftree <jeffrey.ying86@live.com>
2017-05-24 18:04:10 -0700

    Spaceline colors

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -11,12 +11,25 @@
 (global-nlinum-mode t)
 
 (set-face-background 'hl-line "darkblue")
-(set-face-foreground 'linum "grey98")
+(set-face-foreground 'linum "grey42")
 
 (require-package 'spaceline)
 (require 'spaceline-config)
-(setq spaceline-highlight-face-func #'spaceline-highlight-face-evil-state) ; Different highlighting for modes
-(spaceline-spacemacs-theme)
+
 (spaceline-helm-mode)
+(setq spaceline-highlight-face-func #'spaceline-highlight-face-evil-state) ; Different highlighting for modes
+(setq powerline-default-separator 'wave)
+
+(spaceline-toggle-minor-modes-off) ; Saves diminish
+(spaceline-toggle-hud-off) ; Bar at very right
+
+(set-face-attribute 'spaceline-evil-emacs nil :background "#5fd7ff")
+(set-face-attribute 'spaceline-evil-insert nil :background "#f0d817")
+(set-face-attribute 'spaceline-evil-motion nil :background "#ae81ff")
+(set-face-attribute 'spaceline-evil-normal nil :background "#a6e22e")
+(set-face-attribute 'spaceline-evil-replace nil :background "#f92672")
+(set-face-attribute 'spaceline-evil-visual nil :background "#fd971f")
+
+(spaceline-spacemacs-theme)
 
 (provide 'init-theme)
