8c06ebee38c494ae62cfd5c12ff1052a9066fb80
Jefftree <jeffrey.ying86@live.com>
2017-05-31 21:25:36 -0700

    Remove unnecessary bindings

--- lisp/init-bindings.el
+++ lisp/init-bindings.el
@@ -35,8 +35,6 @@
     (", x" 'evil-save-modified-and-close)
     (", v" (kbd "C-w v C-w l") "vsplit")
     (", s" (kbd "C-w s C-w j") "ssplit")
-    (", g" 'magit-status)
-    (", b" 'magit-blame)
 
     ("j" 'evil-next-visual-line)
     ("k" 'evil-previous-visual-line)
