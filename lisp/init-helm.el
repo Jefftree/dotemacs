953f4874809a20f81e61f4ca7bcfd416875c6b72
Jefftree <jeffrey.ying86@live.com>
2017-06-20 22:59:44 -0700

    Helm clean up config

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -2,7 +2,7 @@
 (require 'helm-config)
 (helm-mode 1)
 
-(setq helm-autoresize-min-height 15)
+(setq helm-autoresize-min-height 30)
 (setq helm-autoresize-max-height 30)
 (helm-autoresize-mode t)
 
@@ -33,17 +33,23 @@
 (setq helm-swoop-use-line-number-face t)
 (setq helm-swoop-speed-or-color t)
 
-(require-package 'helm-projectile)
+;; Shorten window height and remove the [C-j] blob
+(setq helm-display-header-line nil)
+(set-face-attribute 'helm-source-header nil :height 0.1)
+
+(require-package 'projectile)
 
 (setq projectile-cache-file (concat dotemacs-cache-directory "projectile.cache"))
 (setq projectile-known-projects-file (concat dotemacs-cache-directory "projectile-bookmarks.eld"))
 
 (setq projectile-completion-system 'helm)
-(helm-projectile-on)
 
 (setq projectile-use-git-grep 1)
 (setq projectile-enable-caching t)
 (setq projectile-file-exists-remote-cache nil)
 (projectile-mode)
+
+(require-package 'helm-projectile)
+(helm-projectile-on)
 
 (provide 'init-helm)
