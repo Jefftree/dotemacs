770f703f4952d8af379bb2aa662ce47947636a53
Jefftree <jeffrey.ying86@live.com>
2017-06-25 21:51:47 -0700

    org init

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,6 +1,15 @@
-  (setq org-ellipsis " …")
+(setq org-ellipsis " …") ;; Shorter ellipses
 
-  (setq org-todo-keywords
-        '((sequence "TODO" "DOING" "|" "DONE")))
+(setq org-todo-keywords
+        '((sequence "TODO(t)" "BLOCKED(b)" "STARTED(s)"
+                    "|" "CANCELLED(c)" "DONE(x)")))
+
+(setq org-adapt-indentation nil) ;; Prevent newline from indenting to bullet
+(setq org-hide-leading-stars t) ;; Hide leading *** bullets to avoid clutter
+(setq org-src-fontify-natively t) ;; Org Mode syntax highlighting minor mode
+
+
+(setq org-default-notes-file (concat user-emacs-directory "notes.org"))
+(setq org-log-done 'time) ;; Add timestamp when tasks are DONE
 
 (provide 'init-org)
