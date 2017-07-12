2e2137b7270a881bd6ceee24581e276edcd65246
Jefftree <jeffrey.ying86@live.com>
2017-07-11 23:35:52 -0700

    Cleanup

--- lisp/init-util.el
+++ lisp/init-util.el
@@ -1,27 +1,3 @@
-(require-package 'exec-path-from-shell)
-(setq exec-path-from-shell-check-startup-files nil)
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
-
-(require 'recentf)
-(setq recentf-save-file (concat dotemacs-cache-directory "recentf"))
-(setq recentf-max-menu-items 500)
-(setq recentf-max-saved-items 1000)
-(setq recentf-auto-cleanup 300)
-(add-hook 'after-init-hook #'recentf-mode)
-
-(require 'tramp)
-(setq tramp-default-method "rsync")
-(setq tramp-completion-reread-directory-timeout nil)
-(setq tramp-persistency-file-name (concat dotemacs-cache-directory "tramp"))
-
-(defun add-ssh-agent-to-tramp ()
-  (cl-pushnew '("-A")
-              (cadr (assoc 'tramp-login-args
-                           (assoc "ssh" tramp-methods)))
-              :test #'equal))
-(add-ssh-agent-to-tramp)
-
 (require 'desktop)
 (setq desktop-dirname dotemacs-cache-directory)
 (add-to-list 'desktop-path dotemacs-cache-directory)
