894fb6dcc66e60184724ce449dc207b046abaa0c
Jefftree <jeffrey.ying86@live.com>
2019-11-21 16:27:24 -0800

    lsp is magical

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -4,6 +4,18 @@
 
 (setq js-indent-level 2) ; Javscript indentation: 2
 
+(require-package 'go-mode)
+
+(require-package 'lsp-mode)
+(add-hook 'go-mode-hook #'lsp-deferred)
+
+(require-package 'lsp-ui)
+(require 'lsp-ui)
+(add-hook 'lsp-mode-hook 'lsp-ui-mode)
+(setq lsp-ui-peek-enable 1)
+(setq lsp-ui-peek-list-width 60)
+(setq lsp-ui-peek-peek-height 40)
+
 ;; iPython interpreter
 (setq python-shell-interpreter "ipython2"
     python-shell-interpreter-args "--simple-prompt -i")
