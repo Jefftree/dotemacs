434c23aca297e093f88c8e64ffc3556e69befebc
Jefftree <jeffrey.ying86@live.com>
2022-06-09 19:40:46 +0000

    LSP fixes

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -3,6 +3,12 @@
 (setq markdown-enable-math 1)
 
 (setq js-indent-level 2) ; Javscript indentation: 2
+(global-undo-tree-mode)
+(evil-set-undo-system 'undo-tree)
+
+(when noninteractive
+  (after! undo-tree
+    (global-undo-tree-mode -1)))
 
 
 
@@ -14,34 +20,35 @@
 (add-hook 'go-mode-hook #'lsp-deferred)
 (add-hook 'python-mode-hook #'lsp-deferred)
 
-;; (require-package 'lsp-jedi)
-;;   (with-eval-after-load "lsp-mode"
-;;     (add-to-list 'lsp-disabled-clients 'pyls)
-;;     (add-to-list 'lsp-enabled-clients 'jedi))
+(after 'lsp-mode (lsp-register-custom-settings
+ '(("gopls.allowImplicitNetworkAccess" t t)
+   )))
 
+(setq lsp-enable-file-watchers nil)
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
 
 ;; (add-hook 'emacs-lisp-mode-hook #'lsp-deferred)
 
 (require-package 'lsp-ui)
 (require 'lsp-ui)
-(add-hook 'lsp-mode-hook 'lsp-ui-mode)
-(setq lsp-ui-peek-enable 1)
-(setq lsp-ui-peek-list-width 60)
-(setq lsp-ui-peek-peek-height 40)
 
 (setq lsp-clients-go-library-directories '("/usr/lib"))
 
 (setq lsp-enable-file-watchers nil)
 
 (require-package 'treemacs)
-
 (require-package 'treemacs-projectile)
 
 (setq treemacs-is-never-other-window t)
-
 (require-package 'lsp-treemacs)
 
-;; (lsp-treemacs-sync-mode 1)
+(lsp-treemacs-sync-mode 1)
 
 (setq python-shell-interpreter "ipython2"
     python-shell-interpreter-args "--simple-prompt -i")
