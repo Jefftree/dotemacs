ebd4b55709a6ab18210735af0f15281c6058eab0
Jefftree <jeffrey.ying86@live.com>
2022-12-01 15:32:52 +0000

    Improve performance

--- init.el
+++ init.el
@@ -9,8 +9,9 @@
               (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
                 (message "[Emacs initialized in %\.3fs]" elapsed)))))
 
-; Magical
-(setq gc-cons-threshold 777777)
+; 100MB
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
 
 (add-hook 'emacs-startup-hook 'toggle-frame-maximized)
 
